7264f64b5aa4d6b7d9bf92865222233898e35314
Drecs <macdonaldserenje2002@gmail.com>
2026-01-11 22:21:55 +0200

    Added prisma client

--- server/Server.js
+++ server/Server.js
@@ -1,10 +1,13 @@
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") }); // load global .env first
+
+
 const express = require("express");
 const cors = require("cors");
-const authRoutes = require("./routes/authRoutes");
-
-require("dotenv").config({ path: "../.env" });  // your global env
+const authRoutes = require("./src/routes/authRoutes");
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
